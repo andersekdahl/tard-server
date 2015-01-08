158f27c5848b12438673f29147c95ca1980214e8
Anders Ekdahl <anders2.ekdahl@sonymobile.com>
2015-01-08 10:18:53 +0100

    Simple WebSocket server based on sente

--- project.clj
+++ project.clj
@@ -3,10 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring-json "0.2.0"]
-                 [http-kit "2.0.0"]
-                 [ring/ring-devel "1.1.8"]
-                 [compojure "1.1.5"]
-                 [ring-cors "0.1.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"] ; May use any v1.5.1+
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [com.taoensso/sente "1.2.0"]
+                 [com.taoensso/timbre "3.3.1"]
+                 [http-kit "2.1.19"]
+                 [compojure "1.3.1"] ; Or routing lib of your choice
+                 [ring "1.3.2"]
+                 [ring/ring-defaults "0.1.2"]
+                 [com.cognitect/transit-clj "0.8.259"]]
+  
   :main tard-server.core)
--- src/tard_server/core.clj
+++ src/tard_server/core.clj
@@ -1,43 +1,77 @@
 (ns tard-server.core
-  (:use [compojure.core :only (defroutes GET)]
-        ring.util.response
-        ring.middleware.cors
-        org.httpkit.server)
-  (:require [compojure.route :as route]
-            [compojure.handler :as handler]
-            [ring.middleware.reload :as reload]
-            [cheshire.core :refer :all]))
+  (:require
+    [clojure.string :as str]
+    [compojure.core :as comp :refer (defroutes GET POST)]
+    [compojure.route :as route]
+    [ring.middleware.defaults]
+    [hiccup.core :as hiccup]
+    [org.httpkit.server :as http-kit-server]
+    [clojure.core.async :as async :refer (<! <!! >! >!! put! chan go go-loop)]
+    [taoensso.timbre :as timbre]
+    [taoensso.sente :as sente]
+    [ring.middleware.anti-forgery :as ring-anti-forgery]
+    [taoensso.sente.packers.transit :as sente-transit]))
 
-(def clients (atom {}))
+(defn- logf [fmt & xs] (println (apply format fmt xs)))
 
-(defn ws
-  [req]
-  (with-channel req con
-    (swap! clients assoc con true)
-    (println con " connected")
-    (on-close con (fn [status]
-                    (swap! clients dissoc con)
-                    (println con " disconnected. status: " status)))))
+(def packer
+  (sente-transit/get-flexi-packer :edn))
 
-(future (loop []
-          (doseq [client @clients]
-            (send! (key client) (generate-string
-                                 {:happiness (rand 10)})
-                   false))
-          (Thread/sleep 5000)
-          (recur)))
+(let [{:keys [ch-recv send-fn ajax-post-fn 
+              ajax-get-or-ws-handshake-fn connected-uids]}
+    (sente/make-channel-socket! {:packer packer})]
+  (def ring-ajax-post ajax-post-fn)
+  (def ring-ajax-get-or-ws-handshake ajax-get-or-ws-handshake-fn)
+  (def ch-chsk ch-recv)
+  (def chsk-send! send-fn)
+  (def connected-uids connected-uids))
 
 (defroutes routes
-  (GET "/happiness" [] ws))
+  (GET "/chsk" req (ring-ajax-get-or-ws-handshake req))
+  (POST "/chsk" req (ring-ajax-post req)))
 
-(def application (-> (handler/site routes)
-                     reload/wrap-reload
-                     (wrap-cors
-                      :access-control-allow-origin #".+")))
+(def ring-handler
+  (let [ring-defaults-config
+        (assoc-in ring.middleware.defaults/site-defaults [:security :anti-forgery]
+          {:read-token (fn [req] (-> req :params :csrf-token))})]
+    (ring.middleware.defaults/wrap-defaults routes ring-defaults-config)))
 
-(defn -main [& args]
-  (let [port (Integer/parseInt 
-               (or (System/getenv "PORT") "8080"))]
-    (run-server application {:port port :join? false})))
+(defmulti event-msg-handler :id)
 
+(defn event-msg-handler* [{:as ev-msg :keys [id ?data event]}]
+  (logf "Event: %s" event)
+  (event-msg-handler ev-msg))
 
+(defmethod event-msg-handler :default [{:as ev-msg :keys [event id ?data ring-req ?reply-fn send-fn]}]
+  (let [session (:session ring-req)
+        uid (:uid session)]
+    (logf "Unhandled event: %s" event)
+    (when ?reply-fn
+      (?reply-fn {:umatched-event-as-echoed-from-from-server event}))))
+
+(defonce http-server_ (atom nil))
+
+(defn stop-http-server! []
+  (when-let [stop-f @http-server_]
+    (stop-f :timeout 100)))
+
+(defn start-http-server! []
+  (stop-http-server!)
+  (let [s (http-kit-server/run-server (var ring-handler) {:port 0})
+        uri (format "http://localhost:%s/" (:local-port (meta s)))]
+    (reset! http-server_ s)
+    (logf "Http-kit server is running at `%s`" uri)))
+
+(defonce router_ (atom nil))
+
+(defn stop-router! [] 
+  (when-let [stop-f @router_] (stop-f)))
+
+(defn start-router! []
+  (stop-router!)
+  (reset! router_ (sente/start-chsk-router! ch-chsk event-msg-handler*)))
+
+(defn -main []
+  (start-router!)
+  (start-http-server!))
+  
